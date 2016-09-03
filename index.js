bac251e38636751b7b13c301272ebb21f0a7ac09
Huy Dang Lê-Ngô <huy-dang.le-ngo@polymtl.ca>
2016-09-03 01:09:48 -0400

    Fix the production server

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 require('babel-register'); // eslint-disable-line import/no-extraneous-dependencies
-const { isDevelopment, isTest } = require('./configuration');
+const { isTest } = require('./configuration');
 const { setupNodeWebpack } = require('./node-webpack');
 const configuration = require('./webpack.config');
 
 setupNodeWebpack(Object.assign({}, configuration, { plugins: [] }));
-if (isDevelopment) {
+if (!isTest) {
   require('./src/server').default(); // eslint-disable-line global-require
-} else if (isTest) {
+} else {
   require('./tests').default(); // eslint-disable-line global-require
 }
