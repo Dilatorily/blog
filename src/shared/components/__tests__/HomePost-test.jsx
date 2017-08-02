63e235bdb981a4802096c7efa03a637d55244c70
Huy Dang Lê-Ngô <ngohu@amazon.com>
2017-08-02 09:43:15 -0700

    Fix the unit tests

--- src/shared/components/__tests__/HomePost-test.jsx
+++ src/shared/components/__tests__/HomePost-test.jsx
@@ -6,7 +6,7 @@ import HomePost from '../HomePost';
 describe('<HomePost />', () => {
   it('should render properly', () => {
     const post = '<h1>Y2K Bugs</h1>';
-    const wrapper = shallow(<HomePost post={post} date="2000-01-01T00:00:00.000-05:00" />);
+    const wrapper = shallow(<HomePost post={post} date="2000-01-01T00:00:00.000-12:00" />);
     expect(wrapper.containsMatchingElement(<h2>Y2K Bugs</h2>)).toBe(true);
     expect(wrapper.containsMatchingElement(<h3>January 1st, 2000</h3>)).toBe(true);
   });
