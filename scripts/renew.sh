dffe68934d677a4f40a05e55370b7549410d7e68
Huy Dang Lê-Ngô <ngohu@amazon.com>
2018-12-28 08:16:30 -0500

    Use systemd to manage the website

--- scripts/renew.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-certbot renew --quiet --pre-hook "/home/ubuntu/fictional-octo-spork/scripts/stop.sh" --post-hook "/home/ubuntu/fictional-octo-spork/scripts/start.sh"
--- scripts/stop.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd /home/ubuntu/fictional-octo-spork
-yarn stop
