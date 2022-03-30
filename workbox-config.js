644f6defe16f6cf9548953f02c6978a3d8b03934
Huy Dang Lê-Ngô <hlengo@slack-corp.com>
2022-03-30 13:11:26 -0700

    Don't build source maps for the service worker

--- workbox-config.js
+++ workbox-config.js
@@ -2,5 +2,6 @@ module.exports = {
   globDirectory: 'docs/',
   globIgnores: ['404.html'],
   globPatterns: ['**/*.{html,png,jpg,xml,ico,json,svg,css}'],
+  sourcemap: false,
   swDest: 'docs/sw.js',
 };
